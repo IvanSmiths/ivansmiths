745fe9549611a88049049c9d852c8e9b53cc2c61
IvanSmiths <info@ivansmiths.com>
2020-12-19 19:52:43 +0100

    Javascript fixed

--- script.js
+++ script.js
@@ -7,18 +7,6 @@
 //   box.className += ' hidden';
 // });
 
-document
-  .querySelector('#model-viewer')
-  .addEventListener('ar-status', (event) => {
-    if (event.detail.status === 'failed') {
-      const error = document.querySelector('#error');
-      error.classList.remove('hide');
-      error.addEventListener('transitionend', (event) => {
-        error.classList.add('hide');
-      });
-    }
-  });
-
 const hamburger = document.querySelector('.hamburger');
 const navLinks = document.querySelector('.nav-links');
 const links = document.querySelectorAll('.nav-links li');
