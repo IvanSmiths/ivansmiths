03c0ce38988e2bf38c9d96aa6de2c1c91dc2a01d
IvanSmiths <info@ivansmiths.com>
2021-01-28 19:02:46 +0100

    ADD Projects

--- script.js
+++ script.js
@@ -206,6 +206,71 @@ function outsideClick(e) {
   }
 }
 
+const modal2 = document.querySelector('#my-modal2');
+const modalBtn2 = document.querySelector('#modal-btn2');
+const closeBtn2 = document.querySelector('.close2');
+
+modalBtn2.addEventListener('click', openModal2);
+closeBtn2.addEventListener('click', closeModal2);
+window.addEventListener('click', outsideClick2);
+
+function openModal2() {
+  modal2.style.display = 'block';
+}
+
+function closeModal2() {
+  modal2.style.display = 'none';
+}
+
+function outsideClick2(e) {
+  if (e.target == modal2) {
+    modal2.style.display = 'none';
+  }
+}
+const modal3 = document.querySelector('#my-modal3');
+const modalBtn3 = document.querySelector('#modal-btn3');
+const closeBtn3 = document.querySelector('.close3');
+
+modalBtn3.addEventListener('click', openModal3);
+closeBtn3.addEventListener('click', closeModal3);
+window.addEventListener('click', outsideClick3);
+
+function openModal3() {
+  modal3.style.display = 'block';
+}
+
+function closeModal3() {
+  modal3.style.display = 'none';
+}
+
+function outsideClick3(e) {
+  if (e.target == modal3) {
+    modal3.style.display = 'none';
+  }
+}
+
+const modal4 = document.querySelector('#my-modal4');
+const modalBtn4 = document.querySelector('#modal-btn4');
+const closeBtn4 = document.querySelector('.close4');
+
+modalBtn4.addEventListener('click', openModal4);
+closeBtn4.addEventListener('click', closeModal4);
+window.addEventListener('click', outsideClick4);
+
+function openModal4() {
+  modal4.style.display = 'block';
+}
+
+function closeModal4() {
+  modal4.style.display = 'none';
+}
+
+function outsideClick4(e) {
+  if (e.target == modal4) {
+    modal4.style.display = 'none';
+  }
+}
+
 //                  TOGGLE BEGIN
 
 const toggleSwitch = document.querySelector(
